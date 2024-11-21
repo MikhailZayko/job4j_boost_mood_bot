8f4a0319f739585d359672ce8f9dea01a4c18755
Admin <mishavolgodonsk@gmail.com>
2024-11-22 00:35:23 +0300

    Внедрение коллекций | Поставщики контекста [#505300]

--- src/main/java/ru/job4j/bmb/services/RecommendationEngine.java
+++ src/main/java/ru/job4j/bmb/services/RecommendationEngine.java
@@ -5,9 +5,27 @@ import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import ru.job4j.bmb.content.Content;
+import ru.job4j.bmb.content.ContentProvider;
+
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class RecommendationEngine implements BeanNameAware {
+
+    private final List<ContentProvider> contents;
+
+    private static final Random RND = new Random(System.currentTimeMillis());
+
+    public RecommendationEngine(List<ContentProvider> contents) {
+        this.contents = contents;
+    }
+
+    public Content recommendFor(Long chatId, Long moodId) {
+        int index = RND.nextInt(0, contents.size());
+        return contents.get(index).byMood(chatId, moodId);
+    }
 
     @PostConstruct
     public void init() {
