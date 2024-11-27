b4cb732fb24aac4c4347a44ac3ebc1cf83f3eda8
Admin <mishavolgodonsk@gmail.com>
2024-11-27 13:40:24 +0300

    Spring Data JPA [#505298]

--- src/main/java/ru/job4j/bmb/model/User.java
+++ src/main/java/ru/job4j/bmb/model/User.java
@@ -1,21 +1,20 @@
 package ru.job4j.bmb.model;
 
-public class User {
+import jakarta.persistence.*;
+import java.util.Objects;
 
+@Entity
+@Table(name = "mb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "client_id")
     private long clientId;
 
+    @Column(name = "chat_id")
     private long chatId;
-
-    public User(Long id, long clientId, long chatId) {
-        this.id = id;
-        this.clientId = clientId;
-        this.chatId = chatId;
-    }
-
-    public User() {
-    }
 
     public Long getId() {
         return id;
@@ -50,14 +49,11 @@ public class User {
             return false;
         }
         User user = (User) o;
-        return clientId == user.clientId && chatId == user.chatId && id.equals(user.id);
+        return Objects.equals(id, user.id);
     }
 
     @Override
     public int hashCode() {
-        int result = id.hashCode();
-        result = 31 * result + Long.hashCode(clientId);
-        result = 31 * result + Long.hashCode(chatId);
-        return result;
+        return Objects.hash(id);
     }
 }
