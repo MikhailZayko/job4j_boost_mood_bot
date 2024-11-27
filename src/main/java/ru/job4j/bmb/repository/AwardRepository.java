52b5c1776011c6b9d12a572bb2c29b6f6a367e4a
Admin <mishavolgodonsk@gmail.com>
2024-11-27 15:13:41 +0300

    Модели данных [#505302]

--- src/main/java/ru/job4j/bmb/repository/AwardRepository.java
+++ src/main/java/ru/job4j/bmb/repository/AwardRepository.java
@@ -2,12 +2,12 @@ package ru.job4j.bmb.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import ru.job4j.bmb.model.MoodLog;
+import ru.job4j.bmb.model.Award;
 
 import java.util.List;
 
 @Repository
-public interface MoodLogRepository extends CrudRepository<MoodLog, Long> {
+public interface AwardRepository extends CrudRepository<Award, Long> {
 
-    List<MoodLog> findAll();
+    List<Award> findAll();
 }
--- src/main/java/ru/job4j/bmb/repository/MoodContentRepository.java
+++ src/main/java/ru/job4j/bmb/repository/MoodContentRepository.java
@@ -2,12 +2,12 @@ package ru.job4j.bmb.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import ru.job4j.bmb.model.MoodLog;
+import ru.job4j.bmb.model.MoodContent;
 
 import java.util.List;
 
 @Repository
-public interface MoodLogRepository extends CrudRepository<MoodLog, Long> {
+public interface MoodContentRepository extends CrudRepository<MoodContent, Long> {
 
-    List<MoodLog> findAll();
+    List<MoodContent> findAll();
 }
