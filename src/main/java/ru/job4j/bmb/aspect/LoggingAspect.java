440c62d3ace519dd7020a328ba4d88137f2324ef
Admin <mishavolgodonsk@gmail.com>
2025-01-02 19:03:51 +0300

    Концепция AOP [#505309]

--- src/main/java/ru/job4j/bmb/aspect/LoggingAspect.java
+++ src/main/java/ru/job4j/bmb/aspect/LoggingAspect.java
@@ -1,4 +1,28 @@
 package ru.job4j.bmb.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import java.util.Arrays;
+
+@Aspect
+@Component
 public class LoggingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Pointcut("execution(* ru.job4j.bmb.services.*.*(..))")
+    private void serviceLayer() {
+    }
+
+    @Before("serviceLayer()")
+    public void logBefore(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().toShortString();
+        Object[] args = joinPoint.getArgs();
+        LOGGER.info("Method called: {} with arguments: {}", methodName, Arrays.toString(args));
+    }
 }
